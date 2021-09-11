99ab8ab5bec817ecf3679765da8397724cc424e7
ivan <ivan-guita@hotmail.com>
2021-09-11 16:30:59 -0300

    classe mensagem criada

--- processa_envio.php
+++ processa_envio.php
@@ -2,6 +2,33 @@
 
     print_r($_POST);
 
+    class Mensagem{
+
+    	public $para = null;
+    	public $assunto = null;
+    	public $mensagem = null;
+
+    	public function __get($atributo){
+
+    		return $this->atributo;
+
+    	}
+
+
+
+    	public function __set($atributo,$valor){
+
+    		$this->$atributo = $valor;
+
+    	}
+
+
+    	public function mensagemValida(){
+
+    		//
+    	}
+    }
+
 
 
 
