8bb416686d692d3194a910defa61e986b5592440
ivan <ivan-guita@hotmail.com>
2021-09-13 23:43:49 -0300

    segurança de aruivos sigilosos ok

--- processa_envio.php
+++ processa_envio.php
@@ -1,8 +1,17 @@
 <?php
+   
 
-   require '../../app_send_email/processa_envio.php';
+   // IMPORTANDO AS CLASSES
 
-  
+    require '../../app_send_mail/bibliotecas/PHPMailer/Exception.php';
+    require '../../app_send_mail/bibliotecas/PHPMailer/OAuth.php';
+    require '../../app_send_mail/bibliotecas/PHPMailer/PHPMailer.php';
+    require '../../app_send_mail/bibliotecas/PHPMailer/POP3.php';
+    require '../../app_send_mail/bibliotecas/PHPMailer/SMTP.php';
+
+
+   require '../../app_send_mail/processa_envio.php';
+
 
 
 
